3ed71b7d3ab13db67d797fd95965e9e7209d07e6
hussainimtiaz <hussain.imtiaz99@gmail.com>
2019-11-11 21:20:14 +0500

    Views Files Ready.

--- app/src/main/java/com/example/aches/MainActivity.java
+++ app/src/main/java/com/example/aches/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(MainActivity.this,Menu.class);
+                Intent mainIntent = new Intent(MainActivity.this, Menu.class);
                 MainActivity.this.startActivity(mainIntent);
                 MainActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+    }
 }
