2e75d81ee78c0ab7c78be112ee57c99704119fed
sanashakoor009 <sanashakoor009@gmail.com>
2019-11-15 22:10:27 +0500

    n

---
+++ app/src/main/java/com/example/aches/NotificationAdapter.java
@@ -0,0 +1,51 @@
+package com.example.aches;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+    public class NotificationAdapter extends RecyclerView.Adapter <com.example.aches.NotificationAdapter.NotificationViewHolder> {
+        private String[] data;
+
+        public NotificationAdapter(String[] data) {
+            this.data = data;
+        }
+
+        @NonNull
+        @Override
+        public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+            View view=inflater.inflate(R.layout.notification_list,parent,false);
+            return new NotificationViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull NotificationViewHolder holder, int position) {
+            String value=data[position];
+            holder.q_text_value.setText(value);
+            holder.a_text_value.setText(value);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.length;
+        }
+
+        public class NotificationViewHolder extends RecyclerView.ViewHolder {
+            ImageView image_value;
+            TextView q_text_value;
+            TextView a_text_value;
+            public NotificationViewHolder( View itemView) {
+                super(itemView);
+                image_value=itemView.findViewById(R.id.photo);
+                q_text_value=itemView.findViewById(R.id.question);
+                a_text_value=itemView.findViewById(R.id.answer);
+            }
+        }
+    }
