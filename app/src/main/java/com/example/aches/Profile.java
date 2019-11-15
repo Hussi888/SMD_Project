ba7d71060d5e77ad09a5edd5800397260a16be62
sanashakoor009 <sanashakoor009@gmail.com>
2019-11-15 13:50:25 +0500

    n

---
+++ app/src/main/java/com/example/aches/Profile.java
@@ -0,0 +1,30 @@
+package com.example.aches;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Profile extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflator, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View rootView= inflator.inflate(R.layout.activity_profile,container,false);
+        return rootView;
+    }
+
+}
+
